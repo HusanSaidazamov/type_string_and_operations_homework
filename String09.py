f84ae11623cb172a6fd2aa6c1ed8b27aebeac5ea
HusanSaidazamov <saidazamovhusan2218@gmail.com>
2023-12-06 20:20:26 +0500

    added filename

--- String09.py
+++ String09.py
@@ -8,8 +8,6 @@ def main(x1,x2,x3):
     Returns:
         str: return answer.
     """
-    x1=[str(x1)]
-    x2=[str(x2)]
-    x3=[str(x3)]
-    return "["+str(x1)+str(x2)+str(x3) +"]"
+
+    return [x1,x2,x3]
 print(main(1,2,3))
